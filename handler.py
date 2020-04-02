6d60de7331edd6a2867fddba3829fd77b0e0f6c0
Andreas Franzén <andreas@devil.se>
2020-04-02 14:37:32 +0000

    Initial db connection

--- handler.py
+++ handler.py
@@ -1,12 +1,33 @@
-# handler.py
-import numpy as np
+import json
+import psycopg2
+import os
 
-def main(event, context):
-    a = np.arange(15).reshape(3, 5)
+def ping(event, context):
+    connection = None
+    try:
+        connection = psycopg2.connect(os.environ.get('DATABASE_URL'))
+        cursor = connection.cursor()
+        sql = "SELECT * FROM collections"
+        
+        cursor.execute(sql)
+        records = cursor.fetchall() 
 
-    print("Your numpy array:")
-    print(a)
-
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(records, default=str)
+        }
+    except (Exception, psycopg2.Error) as err:
+        response = {
+            "statusCode": 500,
+            "body": json.dumps({"error", err}, default=str)
+        }
+    
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+    return response
 
 if __name__ == "__main__":
-    main('', '')
+    ping('', '')
