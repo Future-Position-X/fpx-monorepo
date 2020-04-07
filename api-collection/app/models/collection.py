927e72946f8767d8f4d117ec2b88e98e9ffe7a41
Tobias <tobias.eriksson@fpx.se>
2020-04-07 15:50:22 +0200

    introduce base model

--- api-collection/app/models/collection.py
+++ api-collection/app/models/collection.py
@@ -1,15 +1,11 @@
 import rapidjson
 from rapidjson import DM_ISO8601
+from app.models.base_model import BaseModel
 
-class Collection(object):
-    __slots__ = ['uuid', 'provider_uuid', 'name',
-                 'created_at', 'updated_at', 'revision', 'is_public']
+class Collection(BaseModel, object):
+
     def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        super().__init__(kwargs, ['name', 'is_public'])
 
-    def as_dict(self):
-        attrs = {}
-        for att in self.__slots__:
-            attrs[att] = getattr(self, att)
-        return attrs
+
+
--- api-collection/app/stores/collection.py
+++ api-collection/app/stores/collection.py
@@ -6,15 +6,12 @@ class CollectionStore(Store):
     def find_all(self):
         try:
             c = self.conn.cursor(cursor_factory=RealDictCursor)
-            # this needs an appropriate table
             c.execute('SELECT * FROM collections')
             rows = c.fetchall()
             result = []
             for row in rows:
                 collection = Collection(**row)
                 result.append(collection.as_dict())
-            print(result)
             return result
         except Exception as e:
-            print(e)
             raise StoreException('error finding all collections', e)
