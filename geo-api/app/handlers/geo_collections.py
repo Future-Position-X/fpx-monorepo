71f30b834f637a55331e59934e606a20cac82ba4
Andreas Franzén <andreas@devil.se>
2020-04-14 13:04:03 +0000

    Refactor api

--- geo-api/app/handlers/geo_collections.py
+++ geo-api/app/handlers/geo_collections.py
@@ -1,14 +1,11 @@
 import rapidjson
 from rapidjson import DM_ISO8601
 
+from app.handlers import response
 from app.services.collection import get_all_collections
 
 
 def index(event, context):
     collections = get_all_collections()
-    response = {
-        "statusCode": 200,
-        "body": rapidjson.dumps([c.as_dict() for c in collections], datetime_mode=DM_ISO8601)
-    }
 
-    return response
+    return response(200, rapidjson.dumps([c.as_dict() for c in collections], datetime_mode=DM_ISO8601))
