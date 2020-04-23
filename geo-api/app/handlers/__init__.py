e7505238258a1372b193b24bd81276aa1b581b2e
Andreas Franzén <andreas@devil.se>
2020-04-23 09:16:58 +0000

    Add cors

--- geo-api/app/handlers/__init__.py
+++ geo-api/app/handlers/__init__.py
@@ -1,5 +1,9 @@
 def response(status_code, payload=None):
     return {
         "statusCode": status_code,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True,
+        },
         "body": payload
     }
