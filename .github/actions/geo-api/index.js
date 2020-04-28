c6edab20b3478cd33df62f10b4041017fa8c34ef
Andreas Franzén <andreas@devil.se>
2020-04-28 08:05:57 +0000

    ..

--- .github/actions/geo-api/index.js
+++ .github/actions/geo-api/index.js
@@ -39,9 +39,11 @@ async function runServerlessDeploy() {
         `echo Running sudo ./deploy.sh ${ARGS}...`,
         'cd geo-api/',
         `sudo -E ls -la "$HOME"`,
+        'echo "Running sls config credentials"',
         `sudo -E sls config credentials --provider aws --key ${AWS_ACCESS_KEY_ID} --secret ${AWS_SECRET_ACCESS_KEY} ${ARGS}`,
         `sudo -E ls -la "$HOME/.aws"`,
         'cat "$HOME/.aws/credentials"',
+        'echo "Running ./deploy.sh"',
         `sudo -E ./deploy.sh ${ARGS}`
     )
 }
