6f95800641484849d54626510089659f57ec821a
warbler <33575360+warbler@users.noreply.github.com>
2020-05-05 09:28:18 +0200

    add tree for collections

--- gia-editor/src/main.js
+++ gia-editor/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
+import Vuetify from 'vuetify/lib'
+
+//import './styles/overrides.sass';
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false
