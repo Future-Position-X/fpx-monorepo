52e8c24652ebe5c8b63d3e7315250998c5063bd8
Andreas Franzén <andreas@devil.se>
2020-05-12 10:53:31 +0000

    Add colors

--- gia-editor/src/components/Tree.vue
+++ gia-editor/src/components/Tree.vue
@@ -1,5 +1,15 @@
 <template>
-  <v-treeview @input="selectionUpdate" selectable :items="items"></v-treeview>
+  <v-treeview @input="selectionUpdate" selectable :items="items">
+        <template v-slot:append="{ item }">
+          <!--
+          <v-icon
+            v-if="item.children"
+            v-text="`mdi-${item.id === 1 ? 'home-variant' : 'folder-network'}`"
+            v-color=
+          ></v-icon>-->
+          <div :style="{'background-color': item.color}">YOLO</div>
+        </template>
+  </v-treeview>
 </template>
 
 <script>
@@ -9,6 +19,11 @@ const groupBy = function(xs, key) {
     return rv;
   }, {});
 };
+const selectColor = function(colorNum, colors) {
+    if (colors < 1) colors = 1; // defaults to one color - avoid divide by zero
+    const saturation = 60 + colorNum % 5 * 10;
+    return "hsl(" + (colorNum * (360 / colors) % 360) + "," + saturation + "%,50%)";
+}
 
 export default {
   props: ["collections"],
@@ -21,16 +36,23 @@ export default {
     collections: function() {
       const items = [];
       let sortedCollections = groupBy(this.collections, "name");
+      let i = 1;
+      const len = Object.keys(sortedCollections).length
+      console.log(len)
       for (let [key, value] of Object.entries(sortedCollections)) {
+        let color = selectColor(i, len);
         items.push({
           id: key,
           name: key,
+          color: color,
           children: value.map(function(e) {
             e.id = e.uuid;
             e.name = e.provider_uuid;
+            e.color = color;
             return e;
           })
         });
+        i++;
       }
       this.items = items;
     }
