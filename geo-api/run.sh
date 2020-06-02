41625b7e229130a073905c62c6ca84a28c4c3be2
warbler <33575360+warbler@users.noreply.github.com>
2020-06-02 11:03:18 +0200

    fix run script

--- geo-api/run.sh
+++ geo-api/run.sh
@@ -4,4 +4,4 @@ docker run --rm -p 56733:80 \
   --env DATABASE_URL=postgresql://master:lN1Pb60MO6sC@gia-dev.cjesin4yac8j.eu-north-1.rds.amazonaws.com:5432/gia \
   --env JWT_SECRET=mRJZQrLE6HlStXd4eEQcMLNDDIltgo1eYUzA5TbAcaRlwCX6FI2SLYKjgq19 \
   --name=${app} \
-  -v $PWD:/app ${app}
+  -v "/${PWD}:/app" ${app}
