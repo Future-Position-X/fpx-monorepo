da51f7c6da1baa9670fd6a4f18645f1c0fd448c9
Andreas Franzén <andreas@devil.se>
2020-06-02 15:25:23 +0200

    Implement more endpoints for flask

--- geo-api/app/__init__.py
+++ geo-api/app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET')
+app.config['JWT_IDENTITY_CLAIM'] = 'sub'
 jwt = JWTManager(app)
-from app.handlers.flask import geo_collections
+from app.handlers.flask import geo_collections, geo_items, handler
--- geo-api/app/handlers/flask/__init__.py
+++ geo-api/app/handlers/flask/__init__.py
@@ -2,6 +2,7 @@ from app import app
 from flask import request
 import jwt
 import os
+from flask_jwt_extended import get_jwt_claims
 
 def response(status_code, payload=None):
     response = app.response_class(
@@ -12,29 +13,6 @@ def response(status_code, payload=None):
     return response
 
 def get_provider_uuid_from_request():
-    provider_uuid = ""
-    return provider_uuid
-
-def auth():
-    print(request.headers)
-    authHeader = request.headers['authorization']
-    if authHeader is None:
-        return
-
-    authParts = authHeader.split(' ')
-    if len(authParts) != 2:
-        return
-
-    if authParts[0].lower() != 'bearer':
-        return
-
-    token = authParts[1]
-
-    try:
-        decoded = jwt.decode(
-            token,
-            os.environ.get('JWT_SECRET'),
-            algorithms=['HS256'])
-    except Exception as e:
-        print(e)
-        return
+    claims = get_jwt_claims()
+    print(claims)
+    return claims['provider_uuid']
