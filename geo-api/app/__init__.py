a6680a97836d9a6804e9668656c257f52efcd63d
Andreas Franzén <andreas@devil.se>
2020-06-03 17:17:53 +0200

    Add restx

--- geo-api/app/__init__.py
+++ geo-api/app/__init__.py
@@ -1,7 +1,16 @@
 import os
 from flask import Flask
+from flask_restx import Api
 from flask_jwt_extended import JWTManager
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+api = Api(app, version='1.0', title='GIA geo api',
+          description='GIA geo api',
+          contact='Magnus Engström',
+          contact_url='http://fpx.se',
+          contact_email='magnus.engstrom@fpx.se'
+          )
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET')
 app.config['JWT_IDENTITY_CLAIM'] = 'sub'
 jwt = JWTManager(app)
