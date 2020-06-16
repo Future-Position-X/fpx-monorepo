d78c73fb38c406e37e277544695b1ed8d69cd072
Andreas Franzén <andreas@devil.se>
2020-06-16 16:33:42 +0200

    Add provider endpoints

--- geo-api/app/handlers/flask/geo_providers.py
+++ geo-api/app/handlers/flask/geo_providers.py
@@ -1,9 +1,11 @@
 import rapidjson
 import base64
 from rapidjson import DM_ISO8601, UM_CANONICAL
+from flask_restx import Resource, fields
 
-from app import app
+from app import app, api
 from app.models.provider import Provider
+from app.models import Provider as ProviderDB
 from app.handlers.flask import (
     response,
     get_provider_uuid_from_request
@@ -16,26 +18,50 @@ from app.services.provider import (
 from flask import request
 from flask_jwt_extended import jwt_required
 
+ns = api.namespace('providers', 'Provider operations')
 
-@app.route('/providers')
-@jwt_required
-def providers_index():
-    providers = get_all_providers()
+provider_model = api.model('Provider', {
+    'uuid': fields.String(description='uuid'),
+    'name': fields.String(description='name'),
+    'revision': fields.String(description='revision'),
+    'created_at': fields.String(description='created_at'),
+    'updated_at': fields.String(description='updated_at'),
+})
+update_provider_model = api.model('Provider', {
+    'name': fields.String(description='name'),
+})
+@ns.route('/')
+class ProviderList(Resource):
+    @jwt_required
+    @ns.doc('list_providers')
+    @ns.marshal_list_with(provider_model)
+    def get(self):
+        providers = ProviderDB.all()
+        return providers
 
-    return response(200, rapidjson.dumps([p.as_dict() for p in providers], datetime_mode=DM_ISO8601, uuid_mode=UM_CANONICAL))
+
+@ns.route('/<uuid:provider_uuid>')
+@ns.response(404, 'Provider not found')
+@ns.param('provider_uuid', 'The provider identifier')
+class ProviderApi(Resource):
+    @jwt_required
+    @ns.doc('get_provider')
+    @ns.marshal_with(provider_model)
+    def get(self, provider_uuid):
+        provider = ProviderDB.find_or_fail(provider_uuid)
+        return provider
 
 
-@app.route('/providers/<provier_uuid>')
-@jwt_required
-def providers_get(provider_uuid):
-    provider = get_provider_by_uuid(provider_uuid)
-    return response(200, rapidjson.dumps(provider.as_dict(), datetime_mode=DM_ISO8601))
-
-
-@app.route('/providers/<provider_uuid>', methods=['PUT'])
-@jwt_required
-def providers_update(provider_uuid):
-    provider_dict = request.json
-    provider = Provider(**provider_dict)
-    update_provider_by_uuid(provider_uuid, provider)
-    return response(204)
+    @jwt_required
+    @ns.doc('update_provider')
+    @ns.expect(update_provider_model)
+    def put(self, provider_uuid):
+        if get_provider_uuid_from_request() != provider_uuid:
+            return '', 403
+        provider_dict = request.get_json()
+        provider_new = Provider(**provider_dict)
+        provider = ProviderDB.find_or_fail(provider_uuid)
+        provider.name = provider_new.name
+        provider.save()
+        provider.session().commit()
+        return '', 204
