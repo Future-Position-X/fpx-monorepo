fbe508f484f4e16d3ccf1b62dffdf9eb564c9ad2
Andreas Franzén <andreas@devil.se>
2020-06-22 11:58:23 +0200

    make ci work

--- geo-api/app/handlers/flask/handler.py
+++ geo-api/app/handlers/flask/handler.py
@@ -1,7 +1,13 @@
-from app import app
+from app import api
 from app.handlers.flask import (
     response,
 )
-@app.route('/health')
-def health():
-    return response(200, "healthy!")
+from flask_restx import Resource
+
+ns = api.namespace('health', 'Api health check')
+
+
+@ns.route('/')
+class Health(Resource):
+    def get(self):
+        return "healthy!", 200
