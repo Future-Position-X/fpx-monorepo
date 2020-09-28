4ca4d8e2dd0cc90fd973955cab4636efe893c31a
Andreas Franzén <andreas@devil.se>
2020-09-28 13:09:52 +0200

    Make build fail on test failures

--- geo-api-fastapi/test.sh
+++ geo-api-fastapi/test.sh
@@ -1,11 +1,14 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 # Exit in case of error
 set -e
+set -x
 
 docker-compose -f docker-test-stack.yml build
 docker-compose -f docker-test-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 docker-compose -f docker-test-stack.yml up -d
 docker-compose -f docker-test-stack.yml exec -T backend bash /app/scripts/lint.sh
+echo $?
 docker-compose -f docker-test-stack.yml exec -T backend bash /app/tests-start.sh
+echo $?
 docker-compose -f docker-test-stack.yml down -v --remove-orphans
