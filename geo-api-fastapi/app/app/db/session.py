565cde99794794c71ba4ceddeed341f2a26d11ac
Andreas Franzén <andreas@devil.se>
2020-11-03 11:15:39 +0100

    Use lifo to reuse active connections

--- geo-api-fastapi/app/app/db/session.py
+++ geo-api-fastapi/app/app/db/session.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True, echo_pool="debug"
+    settings.SQLALCHEMY_DATABASE_URI,
+    pool_pre_ping=True,
+    echo_pool="debug",
+    pool_use_lifo=True,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
