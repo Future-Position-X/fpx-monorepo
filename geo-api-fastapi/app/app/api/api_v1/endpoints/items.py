c78709e09b3cbb32ebf26b5927f90dd604dc3611
Andreas Franzén <andreas@devil.se>
2020-11-03 14:00:26 +0100

    Use exclude_none

--- geo-api-fastapi/app/app/api/api_v1/endpoints/items.py
+++ geo-api-fastapi/app/app/api/api_v1/endpoints/items.py
@@ -283,7 +283,7 @@ def format_items(
 @router.get(
     "/items",
     response_model=Union[List[schemas.Item], FeatureCollection],
-    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
     responses={200: {"description": "Items requested", "content": items_content()}},
 )
 def get_items(
@@ -333,7 +333,7 @@ def update_items(
 @router.get(
     "/collections/{collection_uuid}/items",
     response_model=Union[List[schemas.Item], FeatureCollection],
-    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
     responses={
         200: {"description": "Collection items requested", "content": items_content()}
     },
@@ -360,7 +360,7 @@ def get_collection_items(
 @router.post(
     "/collections/{collection_uuid}/items",
     response_model=Union[schemas.Item, Feature],
-    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
     status_code=201,
     responses={
         201: {"description": "Create collection item", "content": item_content()}
@@ -421,7 +421,7 @@ def update_collection_items(
 @router.post(
     "/collections/{collection_uuid}/items/replace",
     response_model=Union[List[schemas.Item], FeatureCollection],
-    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
     status_code=201,
     responses={
         201: {
@@ -464,7 +464,7 @@ def replace_collection_items(
 @router.post(
     "/collections/{collection_uuid}/items/bulk",
     response_model=Union[List[schemas.Item], FeatureCollection],
-    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
     status_code=201,
     responses={
         201: {"description": "Create items in collection", "content": items_content()}
@@ -515,7 +515,7 @@ def delete_collection_items(
 @router.get(
     "/collections/{collection_uuid}/items/{item_uuid}",
     response_model=Union[schemas.Item, Feature],
-    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
     status_code=200,
     responses={
         200: {"description": "Get item in collection", "content": items_content()}
@@ -576,7 +576,7 @@ def update_collection_item(
 @router.get(
     "/collections/by_name/{collection_name}/items",
     response_model=Union[List[schemas.Item], FeatureCollection],
-    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
     responses={
         200: {
             "description": "Collection items by collection name",
@@ -605,7 +605,7 @@ def get_collection_items_by_name(
 @router.get(
     "/items/{item_uuid}",
     response_model=Union[schemas.Item, Feature],
-    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
     status_code=200,
     responses={200: {"description": "Get item", "content": item_content()}},
 )
