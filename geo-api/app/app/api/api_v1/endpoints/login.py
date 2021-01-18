a31f9698e847758148a48d954dd96811b4171375
Andreas Franzén <andreas@devil.se>
2021-01-18 14:56:06 +0100

    CHange method

--- geo-api/app/app/api/api_v1/endpoints/login.py
+++ geo-api/app/app/api/api_v1/endpoints/login.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from typing import Any
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Body, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
@@ -49,9 +49,9 @@ def test_token(current_user: models.User = Depends(deps.get_current_user)) -> An
     return current_user
 
 
-@router.post("/password-recovery/{email}/{new_password}", response_model=schemas.Msg)
+@router.post("/password-recovery/{email}", response_model=schemas.Msg)
 def recover_password(
-    email: str, new_password: str, db: Session = Depends(deps.get_db)
+    email: str, new_password: str = Body(...), db: Session = Depends(deps.get_db)
 ) -> Any:
     """
     Password Recovery
@@ -63,14 +63,16 @@ def recover_password(
             status_code=404,
             detail="The user with this username does not exist in the system.",
         )
-    password_reset_token = generate_password_reset_token(email=email, password=new_password)
+    password_reset_token = generate_password_reset_token(
+        email=email, password=new_password
+    )
     send_reset_password_email(
         email_to=user.email, email=email, token=password_reset_token
     )
     return {"msg": "Password recovery email sent"}
 
 
-@router.post("/reset-password/", response_model=schemas.Msg)
+@router.get("/reset-password/", response_model=schemas.Msg)
 def reset_password(token: str, db: Session = Depends(deps.get_db),) -> Any:
     """
     Reset password
